01d72547d39ece8d06d7d822990f97009e5c528c
zhaoliying-sys <62286719+zhaoliying-sys@users.noreply.github.com>
2020-07-01 14:43:40 +0800

    蛇的死亡控制（自残死和碰壁死）、游戏开始、暂停、退出

---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Snake_1._0
+{
+    public partial class Form1 : Form
+    {
+        private Map map;                                              //地图对象
+        private int grade;                                            //游戏级别
+        private bool ren_speed = false;                               //玩家设置速度  
+        public Form1()
+        {
+            InitializeComponent();
+            this.map = new Map(new Point(50,50));                   //地图左上角坐标为（50，50）
+            this.BackColor = Color.AliceBlue; 
+        }
+
+        private void 开始ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;                                    //开始运行游戏
+            if (this.开始ToolStripMenuItem.Text == "开始")            //如果标题为“开始”
+            {
+                this.开始ToolStripMenuItem.Text = "重新开始";         //改为“重新开始”
+            }
+            else
+            {                                               
+                map.ReSetSnake(this.CreateGraphics());                //重新开始游戏
+                map.score = 0;
+            }
+            暂停ToolStripMenuItem.Enabled = true;                    //暂停/继续菜单可用
+        }
+
+        private void 暂停ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (暂停ToolStripMenuItem.Text == "暂停")                //如果菜单原标题为“暂停”
+            {
+                this.timer1.Enabled = false;                         //停止游戏
+                暂停ToolStripMenuItem.Text = "继续";                 //菜单标题改为“继续”
+            }
+            else
+            {                                                       
+                this.timer1.Enabled = true;                         //继续游戏
+                暂停ToolStripMenuItem.Text = "暂停";                //菜单标题改为“暂停”
+            }
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close(); 
+        }
+
+    }
+}
---
+++ Snake.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Collections;
+using Snake_1._0.Interface;
+
+namespace Snake_1._0
+{
+    public class Snake : IShow
+    {
+        ArrayList blockList;
+        private int headNumber;                                       //蛇头序数
+        private Point headPoint;                                      //蛇头位置
+        private int direction = 1;                                    //0,1,2,3分别代表上、右、下、左
+
+        //public Snake()                                                //构造函数
+        //{
+
+        //}
+
+
+        public Point getHeadPoint                                    //获取蛇头位置
+        {
+            get { return headPoint; }
+        }
+
+
+
+        public bool getHitSelf                                       //判断蛇是否碰到自身
+        {
+            get
+            {
+                IEnumerator myEnumerator = blockList.GetEnumerator();//定义并实例化枚举接口
+                try
+                {
+                    while (myEnumerator.MoveNext())                  //通过循环遍历蛇的各块
+                    {
+                        Block b = (Block)myEnumerator.Current;       //读取当前块
+                        if (!b.Ishead && b.Origin.Equals(headPoint)) //当前块不是蛇头且与蛇头位置相同
+                        {
+                            return true;                             //返回true
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine(e.ToString());
+                }
+                return false;                                        //返回false
+            }
+        }
+
+
+    }
+}
