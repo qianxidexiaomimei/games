bddb94514df0eb24e3263f1072a714029285e8f7
易烊千玺的小迷妹 <1748785468>
2020-07-01 14:35:29 +0800

    初始化游戏运动界面，整个运动流程，蛇吃食物，蛇运行的速度

---
+++ Snake-1.0/Model/Snake.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Collections;
+using Snake_1._0.Interface;
+
+namespace Snake_1._0
+{
+    public class Snake : IShow
+    {
+        public void Display(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnDisplay(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
